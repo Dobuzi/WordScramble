c14ff17c8eabe29a574e89e77a5c2dbea91a306e
Dobuzi <kimjw7981@gmail.com>
2020-10-21 17:32:18 +0900

    Initial Commit

---
+++ WordScramble/WordScrambleApp.swift
@@ -0,0 +1,17 @@
+//
+//  WordScrambleApp.swift
+//  WordScramble
+//
+//  Created by 김종원 on 2020/10/21.
+//
+
+import SwiftUI
+
+@main
+struct WordScrambleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
